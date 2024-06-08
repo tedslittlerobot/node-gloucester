3bb79ef91654c286d2c72e316a17f6360f159253
Stefan Horner <stefan.horner@umotif.com>
2024-06-08 21:40:26 +0100

    xo

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,36 +1,36 @@
 // eslint-disable-next-line ava/no-ignored-test-files
 import test from 'ava';
-import { environmentVariableName, getVerbosityFromEnvironmentVariables } from './utils.js';
+import {environmentVariableName, getVerbosityFromEnvironmentVariables} from './utils.js';
 
-test('environmentVariableName variable', async t => {
+test('environmentVariableName variable', t => {
 	t.is(
 		environmentVariableName({variable: 'foo'}),
 		'foo',
 	);
 });
 
-test('environmentVariableName prefix', async t => {
+test('environmentVariableName prefix', t => {
 	t.is(
 		environmentVariableName({prefix: 'foo'}),
 		'FOO_VERBOSITY',
 	);
 });
 
-test('getVerbosityFromEnvironmentVariables matches', async t => {
+test('getVerbosityFromEnvironmentVariables matches', t => {
 	t.is(
 		getVerbosityFromEnvironmentVariables({foo: 'ridiculouslyVerbose', bar: 'bar'}, 'foo'),
 		'ridiculouslyVerbose',
 	);
 });
 
-test('getVerbosityFromEnvironmentVariables variable does not exist', async t => {
+test('getVerbosityFromEnvironmentVariables variable does not exist', t => {
 	t.is(
 		getVerbosityFromEnvironmentVariables({foo: 'foo', bar: 'bar'}, 'baz'),
 		undefined,
 	);
 });
 
-test('getVerbosityFromEnvironmentVariables invalid verbosity', async t => {
+test('getVerbosityFromEnvironmentVariables invalid verbosity', t => {
 	t.throws(
 		() => getVerbosityFromEnvironmentVariables({foo: 'foo', bar: 'bar'}, 'foo'),
 		{message: 'Invalid Verbosity value found from Environment Variable foo=foo'},
