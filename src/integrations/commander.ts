bb8fc6206779cf2dc1725cba54ade89119a7b6c5
Stefan Horner <stefan.horner@umotif.com>
2024-06-18 14:59:52 +0100

    fix verbosity scanning in commander

--- src/integrations/commander.ts
+++ src/integrations/commander.ts
@@ -1,4 +1,4 @@
-import {type Command} from 'commander';
+import {Option, type Command} from 'commander';
 import type GloucesterEvaluator from '../evaluator.class.js';
 
 export default function setupCommanderAndGloucester(program: Command, gloucester: GloucesterEvaluator) {
@@ -8,21 +8,59 @@ export default function setupCommanderAndGloucester(program: Command, gloucester
 	]);
 
 	program
-		.option('--quiet', 'Suppress output')
-		.on('option:quiet', () => {
-			gloucester.verbosity = 'quiet';
-		})
-		.option('-v, --verbose', 'Verbose Mode (Output debugging information)')
-		.on('option:verbose', () => {
-			gloucester.verbosity = 'verbose';
-		})
-		.option('-vv, --super-verbose', 'Super Verbose Mode (Output lots of debugging information)')
-		.on('option:super-verbose', () => {
-			gloucester.verbosity = 'superVerbose';
-		})
-		.option('-vvv, --ridiculously-verbose', 'Ridiculously Verbose Mode (Output lots of debugging information)')
-		.on('option:super-verbose', () => {
-			gloucester.verbosity = 'ridiculouslyVerbose';
+		.addOption(
+			new Option('--quiet', 'Suppress output')
+				.conflicts([
+					'verbose',
+					'superVerbose',
+					'ridiculouslyVerbose',
+				]),
+		)
+		.addOption(
+			new Option('-v, --verbose', 'Verbose Mode (Output debugging information)')
+				.conflicts([
+					'quiet',
+					'superVerbose',
+					'ridiculouslyVerbose',
+				]),
+		)
+		.addOption(
+			new Option('-vv, --super-verbose', 'Super Verbose Mode (Output lots of debugging information)')
+				.conflicts([
+					'quiet',
+					'verbose',
+					'ridiculouslyVerbose',
+				]),
+		)
+		.addOption(
+			new Option('-vvv, --ridiculously-verbose', 'Ridiculously Verbose Mode (Output lots of debugging information)')
+				.conflicts([
+					'quiet',
+					'verbose',
+					'superVerbose',
+				]),
+		)
+		.hook('preAction', async command => {
+			const options = command.opts();
+
+			if (options.ridiculouslyVerbose === true) {
+				gloucester.verbosity = 'ridiculouslyVerbose';
+				return;
+			}
+
+			if (options.superVerbose === true) {
+				gloucester.verbosity = 'superVerbose';
+				return;
+			}
+
+			if (options.verbose === true) {
+				gloucester.verbosity = 'verbose';
+				return;
+			}
+
+			if (options.quiet === true) {
+				gloucester.verbosity = 'quiet';
+			}
 		});
 
 	return program;
